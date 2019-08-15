aa3a1a29f1d974cb530e07424302b8fc266f51dd
Wilson Horrell <wilsonhorrell@gmai.com>
2019-08-15 18:36:02 +0000

    Adding workouts to the titles and making progress.

--- app/controllers/instructor/programs_controller.rb
+++ app/controllers/instructor/programs_controller.rb
@@ -2,27 +2,19 @@ class Instructor::ProgramsController < ApplicationController
 
 
 def new
+	@title = Title.find(params[:title_id])
 	@program = Program.new
 end
 
 def create
-@program = current_user.programs.create(program_params)
-if @course.valid?
-redirect_to instructor_program_path(@program)
-else
-	render :new, status: :unprocessable_entity
+	@title = Title.find(params[:title_id])
+	@program = @title.programs.create(program_params)
+	redirect_to instructor_title_path(@title)
 end
-	end
 
-	def show
-@program = Program.find(params[:id])
-	end
+private
 
-
-	private
-
-	def program_params
-		params.require(:program).permit(:title, :exercise, :sets, :reps, :inensity, :prescribed_load, :actual_load, :tempo, :rest)
-
-	end
+def program_params
+params.require(:program).permit(:exercise, :sets, :reps, :intensity, :prescribed_load, :actual_load, :tempo, :rest, :notes)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
  root 'static_pages#index'
 
+ resources :titles, only: [:index, :show]
  namespace :instructor do 
- 	resources :titles, only: [:new, :create, :show] 
+ 	resources :titles, only: [:new, :create, :show] do 
+ 		resources :programs, only: [:new, :create]
+ 	end
  	end
 
 end
