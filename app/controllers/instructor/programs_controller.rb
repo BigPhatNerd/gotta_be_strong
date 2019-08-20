6a82c28588675362c024b3b4256745feffab901a
Wilson Horrell <wilsonhorrell@gmai.com>
2019-08-20 23:17:08 +0000

    Everything functioning but want to look into Draper gem for adding some logic

--- app/controllers/instructor/programs_controller.rb
+++ app/controllers/instructor/programs_controller.rb
@@ -1,7 +1,33 @@
 class Instructor::ProgramsController < ApplicationController
 	before_action :authenticate_user!
 	
-	
+	def new
+@program = Program.new
+	end
+
+	def create
+		@title = Title.find(params[:title_id])
+		@program = @title.programs.create(program_params)
+		redirect_to instructor_titles_path
+
+	end
+	def edit
+		@program = Program.find(params[:id])
+
+	end
+
+	def update
+		@program = Program.find(params[:id])
+		@program.update_attributes(program_params)
+		redirect_to instructor_titles_path
+
+	end
+	def destroy
+		@program = Program.find(params[:id])
+		@program.destroy
+		redirect_to instructor_titles_path
+
+	end
 
 	private
 
