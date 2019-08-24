e9b960a1d836168ce7b3cab41c6a246a1662eddc
Wilson Horrell <wilsonhorrell@gmai.com>
2019-08-24 11:53:27 +0000

    Timer functional but needs improvement

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,12 +1,48 @@
- function clock() {
-     var myTimer = setInterval(myClock, 1000);
-     var c = <%= program.rest %>;
+var remaining_rest;
+var myTimer;
 
-     function myClock() {
-         document.getElementById("demo").innerHTML = --c;
-         if (c == 0) {
-             clearInterval(myTimer);
-             alert("Reached zero");
-         }
-     }
- }
+
+function clock() {
+
+    var resetButton = document.getElementById("resetButton");
+    myTimer = setInterval(myClock, 1000);
+}
+
+function reset() {
+
+    clearInterval(myTimer);
+    remaining_rest = rest_in_seconds;
+    document.getElementById("demo").innerHTML = remaining_rest;
+
+
+}
+
+function test() {
+    var count = 0;
+    if (count % 2 === 0) {
+        reset();
+        clock();
+        count++;
+
+
+    } else {
+        clock();
+
+
+
+    }
+}
+
+
+function myClock() {
+
+
+    document.getElementById("demo").innerHTML = --remaining_rest;
+
+
+
+    if (remaining_rest == 0) {
+        clearInterval(myTimer);
+        alert("Reached zero");
+    }
+}
