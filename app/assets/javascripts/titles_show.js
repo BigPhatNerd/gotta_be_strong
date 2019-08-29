96d4543418c05f1a19e9c3cccbe3a7f53d68926d
Wilson Horrell <wilsonhorrell@gmai.com>
2019-08-29 18:02:42 +0000

    Timer messed up and so is heroku.

--- app/assets/javascripts/titles_show.js
+++ app/assets/javascripts/titles_show.js
@@ -71,10 +71,7 @@
       }
   }
 
-  function showTimer(count) {
-      document.getElementById('timerButtons' + count).style.display = "block";
-      document.getElementById('showTimer' + count).style.visibility = "hidden";
-  }
+
 
   function startBeep() {
       startSound = new Audio('/audios/start_sound.mp3');
@@ -97,4 +94,9 @@
       document.getElementById("button2" + count).disabled = true;
       document.getElementById("resetButton" + count).disabled = true;
 
+  }
+
+  function showTimer(count) {
+      document.getElementById('timerButtons' + count).style.display = "block";
+      document.getElementById('showTimer' + count).style.visibility = "hidden";
   }
