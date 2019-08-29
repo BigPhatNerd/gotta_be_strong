1b001e786fc09c339f92ce274fbc88906a674eac
Wilson Horrell <wilsonhorrell@gmai.com>
2019-08-29 11:58:07 +0000

    Cannot get the program.rest to load in sequential order.

--- app/assets/javascripts/titles_show.js
+++ app/assets/javascripts/titles_show.js
@@ -1,72 +1,72 @@
-var remaining_rest;
-var myTimer;
+  var remaining_rest;
+  var myTimer;
 
-function clock(count) {
-    var resetButton = document.getElementById("resetButton" + count);
-    myTimer = setInterval(myClock, 1000);
-}
+  function clock(count) {
+      var resetButton = document.getElementById("resetButton" + count);
+      myTimer = setInterval(() => myClock(count), 1000);
+  }
 
-function reset(count) {
-    clearInterval(myTimer);
-    remaining_rest = rest_in_seconds;
-    document.getElementById("demo" + count).innerHTML = remaining_rest;
-    if ("button2" !== null) {
-        document.getElementById("button2" + count).disabled = false;
-    }
-}
+  function reset(count) {
+      clearInterval(myTimer);
+      remaining_rest = rest_in_seconds;
+      document.getElementById("demo" + count).innerHTML = remaining_rest;
+      if ("button2" !== null) {
+          document.getElementById("button2" + count).disabled = false;
+      }
+  }
 
-function initialClock(count) {
-    reset(counter);
-    clock(counter);
-}
+  function initialClock(counter) {
+      reset(counter);
+      clock(counter);
+  }
 
-function myClock(count) {
-    document.getElementById("demo" + count).innerHTML = --remaining_rest;
-    timerColor();
-    if (remaining_rest == 0) {
-        clearInterval(myTimer);
-        alert("Reached zero");
-    }
-}
+  function myClock(count) {
+      document.getElementById("demo" + count).innerHTML = --remaining_rest;
+      timerColor(count);
+      if (remaining_rest == 0) {
+          clearInterval(myTimer);
+          alert("Reached zero");
+      }
+  }
 
-function clearClock(myTimer, count) {
-    clearInterval(myTimer);
-    document.getElementById("button1" + count).disabled = false;
-    document.getElementById("resetButton" + count).disabled = false;
-}
+  function clearClock(myTimer, count) {
+      clearInterval(myTimer);
+      document.getElementById("button1" + count).disabled = false;
+      document.getElementById("resetButton" + count).disabled = false;
+  }
 
-function switchButtons(buttonId, count) {
-    var hideBtn, showBtn;
-    if (buttonId == 'button1' + count) {
-        initialClock();
-        showBtn = 'button2' + count;
-        hideBtn = 'button1' + count;
-        document.getElementById("button" + count).disabled = true;
-        document.getElementById("resetButton" + count).disabled = true;
-    } else {
-        clock();
-        showBtn = 'button2' + count;
-        hideBtn = 'button1' + count;
-        document.getElementById("button2" + count).disabled = true;
-        document.getElementById("resetButton" + count).disabled = true;
-    }
-    document.getElementById(hideBtn).style.display = 'none';
-    document.getElementById(showBtn).style.display = '';
-}
+  function switchButtons(buttonId, count) {
+      var hideBtn, showBtn;
+      if (buttonId.id == 'button1' + count) {
+          initialClock(count);
+          showBtn = 'button2' + count;
+          hideBtn = 'button1' + count;
+          document.getElementById("button1" + count).disabled = true;
+          document.getElementById("resetButton" + count).disabled = true;
+      } else {
+          clock(count);
+          showBtn = 'button2' + count;
+          hideBtn = 'button1' + count;
+          document.getElementById("button2" + count).disabled = true;
+          document.getElementById("resetButton" + count).disabled = true;
+      }
+      document.getElementById(hideBtn).style.display = 'none';
+      document.getElementById(showBtn).style.display = '';
+  }
 
-function disableStartButton(count) {
-    if (document.getElementById('button2' + count).clicked == true) {
-        document.getElementById("button2" + count).disabled = true;
-    }
-}
+  function disableStartButton(count) {
+      if (document.getElementById('button2' + count).clicked == true) {
+          document.getElementById("button2" + count).disabled = true;
+      }
+  }
 
-function timerColor() {
-    if (remaining_rest < 10) {
-        document.getElementById("demo").style.color = "red";
-    }
-}
+  function timerColor(count) {
+      if (remaining_rest < 10) {
+          document.getElementById("demo" + count).style.color = "red";
+      }
+  }
 
-function showTimer(count) {
-    document.getElementById('timerButtons' + count).style.display = "block";
-    document.getElementById('showTimer' + count).style.visibility = "hidden";
-}
+  function showTimer(count) {
+      document.getElementById('timerButtons' + count).style.display = "block";
+      document.getElementById('showTimer' + count).style.visibility = "hidden";
+  }
